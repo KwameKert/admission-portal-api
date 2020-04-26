c9c141cb5a16d92549d9688848a6bdb5a817ca8e
kwamkert <tomriddle477@gmail.com>
2020-04-26 15:39:27 +0000

    mongoose added

---
+++ src/db/mongoose.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const connectionUrl = 'mongodb://127.0.0.1/admission-db';
+
+
+mongoose.connect(connectionUrl,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}, (error, client) =>{{
+
+    if(error){
+        return console.log('unable to connect to database')
+    }
+
+    console.log("Connected correctly")
+}})
+
